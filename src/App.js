e522691626b41e5b666e20056a8e08728d3633aa
Anetweb <anancheva2014@mail.ru>
2022-10-28 20:45:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data.js";
+import'./App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [gift, setGift] = useState(data);
+const removeGift =(id)=> {
+  let newGift= gift.filter(gift => gift.id  !== id)
+  setGift(newGift)
+}
+return(
+  <div>
+    <div className="container">
+    <h1>List of {gift.length} gifts</h1>
+  </div>
+  {gift.map((element => {
+    const {id,gifty,image} = element;
+
+    return(<div key={id}>
+      <div className="container">
+     <h2>{id} - {gifty}</h2>
+      </div>
+
+      <div className="container">
+       <img src={image} width="300px" height="400px"/>
+      </div>
+
+      <div className="container">
+        <button onClick={() => removeGift(id)}>Remove</button>
+      </div>
+
+
+      </div>
+    )
+  }))}
+  <div className="container">
+    <button onClick={() => setGift([])}>delete all</button>
+  </div>
+  </div>
+)
+
+
+
+
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+{
+    id:1,
+    gifty:"IPhone",
+    image:"https://images.unsplash.com/photo-1652721367098-0ecad4cc0370?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGlwaG9uZSUyMDEzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+},
+
+{
+    id:2,
+    gifty:"Flowers",
+    image:"https://images.unsplash.com/photo-1582794543139-8ac9cb0f7b11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Zmxvd2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+},
+
+{
+    id:3,
+    gifty:"Ring",
+    image:"https://images.unsplash.com/photo-1593472129865-f71f62103a47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzIxfHxyaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+},
+
+{
+    id:4,
+    gifty:"Macbook",
+    image:"https://images.unsplash.com/photo-1526570207772-784d36084510?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hY2Jvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+},
+
+{
+    id:5,
+    gifty:"Jacket",
+    image:"https://images.unsplash.com/photo-1551028719-00167b16eac5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8amFja2V0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+},
+
+{
+    id:6,
+    gifty:"Parfume",
+    image:"https://images.unsplash.com/photo-1587305138714-9675ed5c9415?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGFyZnVtZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+},
+
+{
+    id:7,
+    gifty:"Shoes",
+    image:"https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHNob2VzJTIwc2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+}
+
+]
